91a029041d80ce19e8b48819ae721c493296b57b
abdulrahman-2 <152785282+abdulrahman-2@users.noreply.github.com>
2024-04-13 05:52:17 +0000

    m3

--- src/componets/ClintDetails.jsx
+++ src/componets/ClintDetails.jsx
@@ -6,8 +6,8 @@ export const ClintDetails = () => {
   const { client } = useContext(StateContext);
   return (
     <section className="my-5">
-      <h2 className="text-3xl uppercase font-bold">{client.clientName}</h2>
-      <p>{client.clientAddress}</p>
+      <h2 className="text-3xl uppercase font-bold">{client.name}</h2>
+      <p>{client.address}</p>
     </section>
   );
 };
